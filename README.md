d3a87eb7a813a6fadfa9ae167ba3932a3c7ad2b9
isaccanedo <isaccanedo@gmail.com>
2022-07-05 14:38:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# angular2-alert-notifications
+
+Angular 2/5 - Alert (Toaster) Notifications
+
+To see a demo and further details go to https://jasonwatmore.com/post/2017/06/25/angular-2-4-alert-toaster-notifications
---
+++ app/app.module.ts
@@ -0,0 +1,30 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }  from './app.component';
+import { routing }        from './app.routing';
+
+import { AlertComponent } from './_directives/index';
+import { AlertService } from './_services/index';
+import { HomeComponent } from './home/index';
+import { MultiAlertsComponent, MultiAlertsSectionComponent } from './multi-alerts/index';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        routing
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent,
+        MultiAlertsComponent,
+        MultiAlertsSectionComponent
+    ],
+    providers: [
+        AlertService
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
